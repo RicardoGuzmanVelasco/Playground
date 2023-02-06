1209a71db9c0b6ca5e64122f729bdbed0e2e863d
RGV <ricardo.guzman.velasco@outlook.com>
2023-02-06 18:38:21 +0100

    build(mimeyo): create package layout

---
+++ Assets/MiMeYo/Runtime/Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("MiMeYo")]
+[assembly: AssemblyProduct("Domain")]
+[assembly: AssemblyTitle("MiMeYo.Domain")]
+
+[assembly: AssemblyVersion("0.1")]
+
+[assembly: InternalsVisibleTo("MiMeYo.Domain.Tests")]
+
+[assembly: InternalsVisibleTo("MiMeYo.Domain.Editor")]
+
+[assembly: InternalsVisibleTo("MiMeYo.Domain.Editor.Tests")]
---
+++ Assets/MiMeYo/Runtime/Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("MiMeYo")]
+[assembly: AssemblyProduct("Infrastructure")]
+[assembly: AssemblyTitle("MiMeYo.Infrastructure")]
+
+[assembly: AssemblyVersion("0.1")]
+
+[assembly: InternalsVisibleTo("MiMeYo.Infrastructure.Tests")]
+
+[assembly: InternalsVisibleTo("MiMeYo.Infrastructure.Editor")]
+
+[assembly: InternalsVisibleTo("MiMeYo.Infrastructure.Editor.Tests")]
