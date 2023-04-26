063149f7905cb6a928583c42867957f20445b31a
RGV <ricardo.guzman.velasco@outlook.com>
2023-04-27 01:24:13 +0200

    build(EiQ): create layout

---
+++ Assets/EverythingIsQuiet/Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("EverythingIsQuiet")]
+
+[assembly: AssemblyVersion("0.1")]
+
+[assembly: InternalsVisibleTo("EverythingIsQuiet.Tests")]
+
+[assembly: InternalsVisibleTo("EverythingIsQuiet.Editor")]
+
+[assembly: InternalsVisibleTo("EverythingIsQuiet.Editor.Tests")]
