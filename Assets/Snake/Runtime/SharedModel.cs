3f0b1564fbdb4c8c2c0979fbfd991deb8d5d9171
RGV <ricardo.guzman.velasco@outlook.com>
2023-11-25 17:18:01 +0100

    feat: move to any direction in any frame

--- Assets/Snake/Runtime/SharedModel.cs
+++ Assets/Snake/Runtime/SharedModel.cs
@@ -19,6 +19,16 @@ public class SharedModel : MonoBehaviour
             Model = Model.Tick();
         }
     }
-    
-    
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+            Model = Model.LookTowards((0, 1));
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+            Model = Model.LookTowards((0, -1));
+        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+            Model = Model.LookTowards((-1, 0));
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+            Model = Model.LookTowards((1, 0));
+    }
 }
