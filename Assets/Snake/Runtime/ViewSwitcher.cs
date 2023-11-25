5c897bc026b631997383b0ff1a1e8b3dcf4e2f4e
RGV <ricardo.guzman.velasco@outlook.com>
2023-11-25 19:23:47 +0100

    feat: switch view between canvas and two more not-implemented yet

---
+++ Assets/Snake/Runtime/ViewSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ViewSwitcher : MonoBehaviour
+{
+    const int TotalViews = 3;
+    int currentViewIndex = 3;
+    
+    bool CanvasView => currentViewIndex % TotalViews == 0;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            SwitchView();
+    }
+
+    void SwitchView()
+    {
+        currentViewIndex++;
+        
+        FindObjectOfType<Canvas>(includeInactive: true).gameObject.SetActive(CanvasView);
+    }
+}
