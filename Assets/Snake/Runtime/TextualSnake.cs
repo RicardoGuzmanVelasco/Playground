c987fabde3878bfdd9af95eed6270c13ad3f9f09
RGV <ricardo.guzman.velasco@outlook.com>
2023-11-25 18:36:53 +0100

    revert: replace for loops with functional programming
    This reverts commit 6f9f95f1084a89470cf03ad345abc578801bc8af.

--- Assets/Snake/Runtime/TextualSnake.cs
+++ Assets/Snake/Runtime/TextualSnake.cs
@@ -6,7 +6,6 @@ using DG.Tweening;
 using Snake;
 using TMPro;
 using UnityEngine;
-using static System.Linq.Enumerable;
 
 public class TextualSnake : MonoBehaviour
 {
@@ -35,20 +34,18 @@ public class TextualSnake : MonoBehaviour
         TextualView.DOColor(Color.gray, 2f);
     }
 
-    void PrintGame() => TextualView.text = BuildBoardText();
-
-    string BuildBoardText()
+    void PrintGame()
     {
         var result = new StringBuilder();
-        Range(0, SnakeGame.MapSize).Reverse().ToList().ForEach(i => PrintOneRow(result, i));
-        return result.ToString();
-    }
+        for(var y = SnakeGame.MapSize - 1; y >= 0; y--)
+        {
+            for(var x = 0; x < SnakeGame.MapSize; x++)
+                result.Append(Print(x, y, GameOrLastTickWhenIsGameOver()));
 
-    void PrintOneRow(StringBuilder result, int y)
-    {
-        Range(0, SnakeGame.MapSize).ToList()
-            .ForEach(x => result.Append(Print(x, y, GameOrLastTickWhenIsGameOver())));
-        result.AppendLine();
+            result.AppendLine();
+        }
+
+        TextualView.text = result.ToString();
     }
 
     SnakeGame GameOrLastTickWhenIsGameOver() => Game.GameOver ? Game.Undo().Undo() : Game;
