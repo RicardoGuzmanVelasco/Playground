88e7738de54d4e0c26d266a791b41e4d0b08dc12
RGV <ricardo.guzman.velasco@outlook.com>
2023-11-26 01:51:54 +0100

    feat: spawn first fruit on start

--- Assets/Snake/Runtime/Views/WorldViewSnake.cs
+++ Assets/Snake/Runtime/Views/WorldViewSnake.cs
@@ -1,13 +1,32 @@
+using System;
 using Snake.Runtime.Views.WorldView;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Snake.Runtime.Views
 {
     public class WorldViewSnake : MonoBehaviour
     {
+        [SerializeField] GameObject fruitPrefab;
+        
         SnakeGame Game => FindObjectOfType<SharedModel>().Model;
 
-        void Awake()
+        void Awake() => SpawnWorld();
+
+        void Start()
+        {
+            var position = new Vector2(Game.Fruit.X, Game.Fruit.Y);
+            position -= Vector2.one * ( 1 + SnakeGame.MapSize.OtherEdge());
+            SpawnFruitAt(position);
+        }
+
+        void SpawnFruitAt(Vector2 position)
+        {
+            Assert.IsNotNull(fruitPrefab);
+            Instantiate(fruitPrefab, position, Quaternion.identity, transform);
+        }
+
+        static void SpawnWorld()
         {
             FindObjectOfType<Wall>().BuildFor(SnakeGame.MapSize);
             FindObjectOfType<Floor>().BuildFor(SnakeGame.MapSize);
