de01a54e37c8f655125ab3b0293c4a7f353c54e5
RGV <ricardo.guzman.velasco@outlook.com>
2023-11-26 01:32:09 +0100

    feat: spawn random slab

--- Assets/Snake/Runtime/Views/WorldView/Slab.cs
+++ Assets/Snake/Runtime/Views/WorldView/Slab.cs
@@ -1,6 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
 
 internal class Slab : MonoBehaviour
 {
-    
+    [SerializeField] Sprite[] slabSprites;
+
+    void Start()
+    {
+        Assert.IsNotNull(slabSprites);
+        Assert.IsTrue(slabSprites.Any());
+        
+        var randomSlab = slabSprites[UnityEngine.Random.Range(0, slabSprites.Length)];
+        GetComponent<SpriteRenderer>().sprite = randomSlab;
+    }
 }
