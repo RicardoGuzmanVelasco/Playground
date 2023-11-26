6e8106f0b13d3c4bbc126b8cb97d175561c0584a
RGV <ricardo.guzman.velasco@outlook.com>
2023-11-26 02:30:03 +0100

    feat: rotate snake head towards looking direction

--- Assets/Snake/Runtime/Views/WorldView/SnakeHead.cs
+++ Assets/Snake/Runtime/Views/WorldView/SnakeHead.cs
@@ -1,7 +1,16 @@
-﻿namespace Snake.Runtime.Views.WorldView
+﻿using System;
+using UnityEngine;
+
+namespace Snake.Runtime.Views.WorldView
 {
     internal class SnakeHead : SnakePart
     {
-        
+        void Start()
+        {
+            var lookingAt = FindObjectOfType<SharedModel>().Model.direction;
+            var vector = new Vector2(lookingAt.X, lookingAt.Y);
+            
+            transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.left, vector));
+        }
     }
 }
