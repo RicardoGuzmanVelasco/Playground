885bcaa8a9894cdd07b4833d4a422e758ad8db40
RGV <ricardo.guzman.velasco@outlook.com>
2023-12-02 16:39:53 +0100

    build: change to nunit

--- MonoRepos/CommitionalConvents/CommitionalConvents/CommitionalConvents.Tests/UnitTest1.cs
+++ MonoRepos/CommitionalConvents/CommitionalConvents/CommitionalConvents.Tests/UnitTest1.cs
@@ -3,31 +3,37 @@ using static CommitionalConvents.Commit.Type;
 
 namespace CommitionalConvents.Tests;
 
-public class WipTests
+public class Tests
 {
-    [Fact]
+    [Test]
     public void TimeSpent()
     {
         Wip.Begin().TotalTimeSpent.Should().Be(0);
-        
+
         Wip.Begin().Spend(1f, Feat).TotalTimeSpent.Should().Be(1);
         Wip.Begin().Spend(1f, Chore).TimeSpentOn(Chore).Should().Be(1);
         Wip.Begin().Spend(1f, Ci).TimeSpentOn(Fix).Should().Be(0);
-        
+
         Wip.Begin().Spend(1f, Chore, Feat).TotalTimeSpent.Should().Be(2);
-        
+
         Wip.Begin().Spend(887f, Style).Normalize().TimeSpentOn(Style).Should().Be(1);
-        
+
         Wip.Begin().Spend(340f, Build, Docs, Perf).Normalize().TotalTimeSpent.Should().Be(1);
-        Wip.Begin().Spend(340f, Build, Docs, Perf).Normalize().TimeSpentOn(Build).Should().Be(1/3f);
-        Wip.Begin().Spend(340f, Build, Docs, Perf).Normalize().TimeSpentOn(Docs).Should().Be(1/3f); 
-        Wip.Begin().Spend(340f, Build, Docs, Perf).Normalize().TimeSpentOn(Perf).Should().Be(1/3f);
+        Wip.Begin().Spend(340f, Build, Docs, Perf).Normalize().TimeSpentOn(Build).Should().Be(1 / 3f);
+        Wip.Begin().Spend(340f, Build, Docs, Perf).Normalize().TimeSpentOn(Docs).Should().Be(1 / 3f);
+        Wip.Begin().Spend(340f, Build, Docs, Perf).Normalize().TimeSpentOn(Perf).Should().Be(1 / 3f);
     }
 
-    [Fact]
+    [Test]
     public void CommitWip()
     {
         Wip.Begin().Commit().IsNone.Should().BeTrue();
         Wip.Begin().Spend(1.45f, Chore).Commit().IsNone.Should().BeFalse();
+
+        Wip.Begin().Spend(3245, Ci).Commit().Match
+        (
+            Some: commit => throw new NotImplementedException(),
+            None: Assert.Fail
+        );
     }
 }
