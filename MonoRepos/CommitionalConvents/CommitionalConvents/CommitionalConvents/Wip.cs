5733627d28d4b1c336dadcc89e70884eb8012e4a
RGV <ricardo.guzman.velasco@outlook.com>
2023-12-02 15:11:20 +0100

    refactor: compute total time spent by sum

--- MonoRepos/CommitionalConvents/CommitionalConvents/CommitionalConvents/Wip.cs
+++ MonoRepos/CommitionalConvents/CommitionalConvents/CommitionalConvents/Wip.cs
@@ -7,25 +7,21 @@ namespace CommitionalConvents
     {
         Dictionary<CommitType, float> timeSpent = new();
 
-        public float TotalTimeSpent { get; private set; }
+        public float TotalTimeSpent => timeSpent.Values.Sum();
 
         Wip() { }
         public static Wip Begin() => new();
         
         public float TimeSpentOn(CommitType type)
             => timeSpent.GetValueOrDefault(type);
+
+        public Wip Spend(float time, params CommitType[] types)
+            => types.Aggregate(this, (current, type) => current.Spend(time, type));
         
         public Wip Spend(float time, CommitType type)
             => this with
             {
-                timeSpent = new(timeSpent)
-                {
-                    [type] = timeSpent.GetValueOrDefault(type) + time
-                },
-                TotalTimeSpent = TotalTimeSpent + time
+                timeSpent = new(timeSpent) { [type] = timeSpent.GetValueOrDefault(type) + time }
             };
-
-        public Wip Spend(float time, params CommitType[] types)
-            => types.Aggregate(this, (current, type) => current.Spend(time, type));
     }
 }
