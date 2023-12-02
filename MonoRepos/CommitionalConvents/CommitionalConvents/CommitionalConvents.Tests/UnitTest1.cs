ebb48754ff64d572fa043f871a7cfe991a4ee372
RGV <ricardo.guzman.velasco@outlook.com>
2023-12-02 14:40:37 +0100

    refactor: change from commit to commit type

--- MonoRepos/CommitionalConvents/CommitionalConvents/CommitionalConvents.Tests/UnitTest1.cs
+++ MonoRepos/CommitionalConvents/CommitionalConvents/CommitionalConvents.Tests/UnitTest1.cs
@@ -4,10 +4,5 @@ namespace CommitionalConvents.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void EqualityByCommitType()
-    {
-        Commit.Feat().SameTypeThan(Commit.Feat()).Should().BeTrue();
-        Commit.Feat().SameTypeThan(Commit.Fix()).Should().BeFalse();
-    }
+    
 }
---
+++ MonoRepos/CommitionalConvents/CommitionalConvents/CommitionalConvents/CommitType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CommitionalConvents
+{
+    public record CommitType
+    {
+        readonly string type;
+
+        CommitType(string type) => this.type = type;
+
+        public static CommitType Feat() => new("feat");
+        public static CommitType Fix() => new("fix");
+        public static CommitType Refactor() => new("refactor");
+        public static CommitType Test() => new("test");
+        
+        public static CommitType Ci() => new("ci");
+        public static CommitType Build() => new("build");
+        public static CommitType Docs() => new("docs");
+        public static CommitType Chore() => new("chore");
+        
+        public static CommitType Perf() => new("perf");
+        public static CommitType Style() => new("style");
+    }
+}
