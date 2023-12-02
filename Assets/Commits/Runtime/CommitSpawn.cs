21de024a8399916363c811218427a7f9a1d9cd69
RGV <ricardo.guzman.velasco@outlook.com>
2023-12-02 23:51:19 +0100

    feat: log spawned commit

--- Assets/Commits/Runtime/CommitSpawn.cs
+++ Assets/Commits/Runtime/CommitSpawn.cs
@@ -1,9 +1,19 @@
+using System;
+using CommitionalConvents;
 using UnityEngine;
 
 namespace Commits.Runtime
 {
     public class CommitSpawn : MonoBehaviour
     {
-        
+        void Awake()
+        {
+            FindObjectOfType<SharedModel>().StagingCompleted += FreeSpawningCommit;
+        }
+
+        void FreeSpawningCommit(Commit commit)
+        {
+            Debug.Log($"Spawning commit: {commit.CommitType.id} of {commit.TotalSize} size");
+        }
     }
 }
