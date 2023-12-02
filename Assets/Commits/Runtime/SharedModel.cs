3cc8a67a23ef33cb34439200f7bff3e0638940f2
RGV <ricardo.guzman.velasco@outlook.com>
2023-12-02 23:21:02 +0100

    feat: complete the staging by model-dictated time

--- Assets/Commits/Runtime/SharedModel.cs
+++ Assets/Commits/Runtime/SharedModel.cs
@@ -1,4 +1,5 @@
-﻿using CommitionalConvents;
+﻿using System;
+using CommitionalConvents;
 using UnityEngine;
 
 namespace Commits.Runtime
@@ -9,11 +10,31 @@ namespace Commits.Runtime
         
         public Wip Wip { get; private set; } = Wip.Begin();
         
+        public Staging Staging { get; private set; }
+        public bool IsStaging => Staging != null;
+        
         public bool CanCommit => Wip.TotalTimeSpent >= HardcodedConstantToReplace;
         public float ProportionToCommit => Wip.TotalTimeSpent / HardcodedConstantToReplace;
         public float MinTimeToCommit => HardcodedConstantToReplace;
+        
+        void Update() => InjectStaging(Time.deltaTime);
 
         public void Inject(float time, Commit.Type type) => Wip = Wip.Spend(time, type);
-        public void Commit() => Wip = Wip.Begin();
+        public void InjectStaging(float time)
+        {
+            if(!IsStaging)
+                return;
+            
+            Staging = Staging.Inject(time);
+            if(Staging.Done)
+                Staging = null;
+        }
+
+        public void Commit()
+        {
+            Staging = Staging.DoWith(Wip);
+            Wip = Wip.Begin();
+        }
+
     }
 }
